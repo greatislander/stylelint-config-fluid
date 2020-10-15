c18d9da69c2732924b697b533939f8e32a8cf236
Ned Zimmerman <ned@bight.dev>
2020-10-15 10:00:44 -0300

    feat: add configuration from fluid-grunt-lint-all

---
+++ index.js
@@ -0,0 +1,15 @@
+/*
+Copyright the stylelint-config-fluid copyright holders.
+See the AUTHORS.md file at the top-level directory of this distribution and at
+https://github.com/fluid-project/stylelint-config-fluid/raw/main/AUTHORS.md.
+Licensed under the New BSD license. You may not use this file except in compliance with this License.
+You may obtain a copy of the New BSD License at
+https://github.com/fluid-project/stylelint-config-fluid/raw/main/LICENSE.md.
+*/
+
+"use strict";
+
+const path = require("path");
+const configPath = path.join(__dirname, ".stylelintrc.json");
+
+module.exports = require(configPath);
